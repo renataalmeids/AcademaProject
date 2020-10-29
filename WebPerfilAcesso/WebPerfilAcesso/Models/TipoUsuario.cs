025201cf8b5b35238b3addbed8d1a6e413302163
ProjetoCSarpp-Alura <ras_almeida@hotmail.com>
2020-10-29 12:35:47 -0300

    Incluindo Classes

---
+++ WebPerfilAcesso/WebPerfilAcesso/Models/TipoUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPerfilAcesso.Models
+{
+    [Table("TipoUsuario")]
+    public class TipoUsuario
+    {
+        [Display(Name = "Código")]
+
+        [Column("Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "TipoUsuario")]
+        [Column("NomeTipoUsuario")]
+        public string NomeTipoUsuario { get; set; }
+
+    }
+}
